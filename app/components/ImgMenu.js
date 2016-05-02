3c1dc33d3e62c515b64a2f7cd3af7c6da8e4a885
inkblotty <inkblot.and.blush@gmail.com>
2016-05-02 06:08:19 -0600

    added subtle transition when navigating between routes; change animation between carousel clicks

--- app/components/ImgMenu.js
+++ app/components/ImgMenu.js
@@ -1,6 +1,8 @@
 const React = require('react');
 const Link = require('react-router').Link;
 
+const ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+
 const ImgMenu = props => {
 	return (
 		<div>
@@ -12,7 +14,12 @@ const ImgMenu = props => {
 					<span className='fa fa-picture-o' />
 				</Link>
 			</div>
-			{props.children}
+			<ReactCSSTransitionGroup
+				transitionName="appear"
+				transitionEnterTimeout={500}
+				transitionLeaveTimeout={500}>
+				{React.cloneElement(props.children, {key:props.location.pathname})}
+			</ReactCSSTransitionGroup>
 		</div>
 	)
 }
