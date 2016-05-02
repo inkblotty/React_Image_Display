013bf14edab058de12abf20a595d0b0e00ea772f
inkblotty <inkblot.and.blush@gmail.com>
2016-05-02 08:00:57 -0600

    made carousel arrow clicks into state changes instead of route changes for inner css transitions

--- app/components/Carousel.js
+++ app/components/Carousel.js
@@ -3,38 +3,47 @@ const ImgArray = require('../imagesArray');
 const ThumbSlider = require('./ThumbSlider');
 const Link = require('react-router').Link
 
-const Carousel = props => {
-	var currentImg = props.routeParams ? ImgArray[props.routeParams.imgIndex] : ImgArray[0];
+const Carousel = React.createClass({
+	getInitialState: function() {
+		return {
+			currentIndex: 0,
+			currentImg: this.props.routeParams ? ImgArray[this.props.routeParams.imgIndex] : ImgArray[0]
+		}
+	},
+	prevImage: function() {
+		this.setState({
+			currentIndex: this.state.currentIndex > 0 ?
+				this.state.currentIndex - 1 : ImgArray.length - 1,
+			currentImg: ImgArray[this.state.currentIndex]
+		})
+	},
+	nextImage: function() {
+		this.setState({
+			currentIndex: this.state.currentIndex < ImgArray.length-1 ?
+				this.state.currentIndex + 1 : 0,
+			currentImg: ImgArray[this.state.currentIndex]
+		})
+	},
+	render: function() {
+		let currentImg = '../' + this.state.currentImg;
 
-	// previous when on the first img will route to last, etc.
-	let prevImgIndex = Number(props.routeParams.imgIndex) > 0 ?
-		(Number(props.routeParams.imgIndex) - 1).toString() :
-		ImgArray.length - 1;
-	let nextImgIndex = Number(props.routeParams.imgIndex) < ImgArray.length-1 ?
-		(Number(props.routeParams.imgIndex) + 1).toString() :
-		0;
-
-	let prevLink = `/carousel/${prevImgIndex}`;
-	let nextLink = `/carousel/${nextImgIndex}`;
-
-	currentImg = '../' + currentImg;
-
-	return (
-		<div className="carousel-container">
-			<div className="lightbox">
-				<Link to={prevLink} className="arrow left">
-					<i className="fa fa-caret-left" />
-				</Link>
-				<div className="big-img-wrapper">
-					<img className="big-img" src={currentImg} />
+		return (
+			<div className="carousel-container">
+				<div className="lightbox">
+					<div onClick={this.prevImage} className="arrow left">
+						<i className="fa fa-caret-left" />
+					</div>
+					<div className="big-img-wrapper">
+						<img className="big-img" src={currentImg} />
+					</div>
+					<div onClick={this.nextImage} className="arrow right">
+						<i className="fa fa-caret-right" />
+					</div>
 				</div>
-				<Link to={nextLink} className="arrow right">
-					<i className="fa fa-caret-right" />
-				</Link>
+				<ThumbSlider currentImg={currentImg} />
 			</div>
-			<ThumbSlider currentImg={currentImg} />
-		</div>
-	)
-}
+		)
+	}
+});
 
 module.exports = Carousel;
--- app/components/Thumbnail.js
+++ app/components/Thumbnail.js
@@ -2,17 +2,24 @@ const React = require('react');
 const Link = require('react-router').Link;
 
 const Thumbnail = props => {
-	let pathname = props.fromCarousel === 'true' ? 
-		`/carousel/${props.imgIndex}` : 
-		`carousel/${props.imgIndex}`;
+	let pathname = `carousel/${props.imgIndex}`;
 
-	return (
-		<div className="thumb-wrapper">
-			<Link to={pathname} >
-			<img src={props.imgSrc} className="thumb-img" />
-			</Link>
-		</div>
-	)
+	if (!props.fromCarousel) {
+		return (
+			<div className="thumb-wrapper">
+				<Link to={pathname} >
+				<img src={props.imgSrc} className="thumb-img" />
+				</Link>
+			</div>
+		)
+	}
+	else {
+		return (
+			<div className="thumb-wrapper">
+				<img src={props.imgSrc} className="thumb-img" />
+			</div>
+		)
+	}
 }
 
 module.exports = Thumbnail;
